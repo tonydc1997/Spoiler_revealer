13d42180aca1009b383f80273be3c86a4bd493a0
Anthony Curtis <tonydc1997@gmail.com>
2018-03-29 10:18:21 -0400

    Finish basic spoiler functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-
-
+// Hide spoiler text 
+$('.spoiler span').hide();
+// When button is pressed 
+$('.spoiler button').click( () => {
+  // Show the spoiler text
+  $('.spoiler span').show();
+  // Hide the Reveal Button
+  $('.spoiler button').show();
+});
+ 
