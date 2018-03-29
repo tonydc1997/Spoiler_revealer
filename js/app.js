e10b8dcfa54bd043cd27004a9833279e45c0b63c
Anthony Curtis <tonydc1997@gmail.com>
2018-03-29 10:35:43 -0400

    Add dynamic button to page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
+// Create Reveal spoiler button
+const $button = $('<button>Reveal Spoiler</button>');
+// Append to the page
+$('.spoiler').append($button);
+
 // Hide spoiler text 
 $('.spoiler span').hide();
 // When button is pressed 
-$('.spoiler button').click( () => {
+$button.click( () => {
   // Show the spoiler text
-  $('.spoiler span').show();
+  $('.spoiler span').fadeIn(1000);
   // Hide the Reveal Button
-  $('.spoiler button').show();
+  $button.hide();
 });
  
